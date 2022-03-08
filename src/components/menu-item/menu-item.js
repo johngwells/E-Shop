07eb9b1c3bd466d1f0cd2fd8038d43f9b35425de
John Wells <molekewl2017@gmail.com>
2022-03-08 01:02:35 -0800

    adjust css layout for images

--- src/components/menu-item/menu-item.js
+++ src/components/menu-item/menu-item.js
@@ -1,8 +1,8 @@
 const MenuItem = ({ title, subtitle, imageUrl }) => (
   <div className='border w-3/6 h-80 p-3.5'>
-    <div className='border flex justify-center items-center h-72 overflow-hidden relative'>
+    <div className='flex justify-center items-center h-72 rounded-lg overflow-hidden relative'>
       <img src={imageUrl} className='object-contain absolute z-10' />
-      <div className='border border-gray-600 rounded-lg w-36 h-24 flex justify-center items-center flex-col relative z-20 bg-violet-100'>
+      <div className='rounded-lg w-36 h-24 flex justify-center items-center flex-col relative z-20 bg-violet-100 opacity-70'>
         <span className='text-4xl font-Hubballi'>{title}</span>
         <span className='text-2xl italic font-Hubballi'>{subtitle}</span>
       </div>
