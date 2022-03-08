11b10da7e6da533cab4a1dae5eb923a6691fea59
John Wells <molekewl2017@gmail.com>
2022-03-07 23:30:13 -0800

    change h1 to spans. change main bg color

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -6,7 +6,7 @@ const HomePage = () => {
       <div className='border w-3/6 h-80 p-3.5'>
         <div className='border flex justify-center items-center h-72'>
           <div className='border w-24 h-24 flex justify-center items-center flex-col'>
-            <h1 className='text-3xl font-Hubballi'>Title</h1>
+            <span className='text-3xl font-Hubballi'>Title</span>
             <span className='text-2xl italic font-Hubballi'>Subtitle</span>
           </div>
         </div>
@@ -14,7 +14,7 @@ const HomePage = () => {
       <div className='border w-3/6 h-80 p-3.5'>
         <div className='border flex justify-center items-center h-72'>
           <div className='border w-24 h-24 flex justify-center items-center flex-col'>
-            <h1 className='text-3xl font-Hubballi'>Title</h1>
+            <span className='text-3xl font-Hubballi'>Title</span>
             <span className='text-2xl italic font-Hubballi'>Subtitle</span>
           </div>
         </div>
@@ -22,7 +22,7 @@ const HomePage = () => {
       <div className='border w-3/6 h-80 p-3.5'>
         <div className='border flex justify-center items-center h-72'>
           <div className='border w-24 h-24 flex justify-center items-center flex-col'>
-            <h1 className='text-3xl font-Hubballi'>Title</h1>
+            <span className='text-3xl font-Hubballi'>Title</span>
             <span className='text-2xl italic font-Hubballi'>Subtitle</span>
           </div>
         </div>
@@ -30,7 +30,7 @@ const HomePage = () => {
       <div className='border w-3/6 h-80 p-3.5'>
         <div className='border flex justify-center items-center h-72'>
           <div className='border w-24 h-24 flex justify-center items-center flex-col'>
-            <h1 className='text-3xl font-Hubballi'>Title</h1>
+            <span className='text-3xl font-Hubballi'>Title</span>
             <span className='text-2xl italic font-Hubballi'>Subtitle</span>
           </div>
         </div>
