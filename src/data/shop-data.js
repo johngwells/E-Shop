14fb082203f9a9da17e6d7b09b173f7ae1607a19
John Wells <molekewl2017@gmail.com>
2022-03-11 23:10:26 -0800

    create temporary data file to work with products

---
+++ src/data/shop-data.js
@@ -0,0 +1,54 @@
+const SHOP_DATA = [
+  {
+    id: 1,
+    title: 'Fujifilm',
+    route: 'fujifilm',
+    items: [
+      {
+        id: 1,
+        name: 'X-T4',
+        imageUrl: '',
+        price: 1499
+      }
+    ]
+  },
+  {
+    id: 2,
+    title: 'Sony',
+    route: 'sony',
+    items: [
+      {
+        id: 1,
+        name: 'A7 IV',
+        imageUrl: '',
+        price: 2499
+      }
+    ]
+  },
+  {
+    id: 3,
+    title: 'Nikon',
+    route: 'nikon',
+    items: [
+      {
+        id: 1,
+        name: 'Z fc',
+        imageUrl: '',
+        price: 1199
+      }
+    ]
+  },
+  {
+    id: 4,
+    title: 'Canon',
+    route: 'canon',
+    items: [
+      {
+        id: 1,
+        name: 'EOS M6',
+        imageUrl: '',
+        price: 1099
+      }
+    ]
+  }
+]
