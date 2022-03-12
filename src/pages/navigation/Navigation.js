0927db1abf01ee6bbfd4ac2dfe2b2fff8aeb3e7b
John Wells <molekewl2017@gmail.com>
2022-03-12 13:34:20 -0800

    Logo route back to home / and route to shop page link. Hover over color change

--- src/pages/navigation/Navigation.js
+++ src/pages/navigation/Navigation.js
@@ -1,10 +1,13 @@
 import SearchBar from '../../components/searchbar/SearchBar';
+import { Link } from 'react-router-dom';
 
 const Navigation = () => (
   <>
     <nav className='mb-6 flex flex-row'>
       <div className='w-full'>
-        <h1 className='text-5xl font-Hubballi text-indigo-700'>Aperture</h1>
+        <Link to='/'>
+          <h1 className='text-5xl font-Hubballi text-indigo-700'>Aperture</h1>
+        </Link>
       </div>
       <div className='flex ml-28 mt-3 w-full'>
         <SearchBar />
@@ -18,7 +21,9 @@ const Navigation = () => (
     </nav>
     <nav className='ml-11 bg-slate-200 mr-11'>
       <ul className='flex flex-row p-2'>
-        <li className='p-1 font-Hubballi text-xl'>Shop</li>
+        <Link to='/shop' className='p-1 font-Hubballi text-xl hover:text-indigo-700'>
+          Shop
+        </Link>
         <li className='p-1 font-Hubballi text-xl'>Category</li>
         <li className='p-1 font-Hubballi text-xl'>Brands</li>
       </ul>
