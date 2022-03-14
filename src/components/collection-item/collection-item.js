b32dc20154406746acdd69788d0c606e47337bd4
John Wells <molekewl2017@gmail.com>
2022-03-14 07:14:40 -0700

    css button on hover

--- src/components/collection-item/collection-item.js
+++ src/components/collection-item/collection-item.js
@@ -10,7 +10,7 @@ const CollectionItem = ({ name, imageUrl, price }) => (
     </div>
     <div>
       <div className='invisible absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 group-hover:visible'>
-        <button className='btn btn-active h-8 w-32 rounded-md px-0 opacity-90 hover:opacity-100'>
+        <button className='btn btn-xs btn-active h-8 w-32 rounded-md px-0  hover:opacity-100 opacity-80'>
           Add to cart
         </button>
       </div>
